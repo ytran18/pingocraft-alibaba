90bc1b9c8175a08dba3458d604f3067298338da8
jineu <a11718trannhuy@gmail.com>
2022-09-24 12:59:43 +0700

    slide show

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,28 @@
 import classnames from 'classnames/bind'
 import styles from './Home.module.scss'
 
+import {Box, Carousel, Image} from 'grommet'
+
 import slideImg1 from '../../assets/slider/slider1.jpeg'
 import slideImg2 from '../../assets/slider/slider2.jpeg'
 import slideImg3 from '../../assets/slider/slider3.jpeg'
 
 const cx = classnames.bind(styles)
 
-function Home() {
+function Home() 
+{
+
     return (
         <div className={cx('container')}>
             
             {/* slide show */}
-            <div className={cx('slide-show')}>
-                <div className={cx('mySlides', 'fade')}>
-                    <img alt='img-1' src={slideImg1}/>
-                </div>
-
-                <div className={cx('mySlides', 'fade')}>
-                    <img alt='img-2' src={slideImg2}/>
-                </div>
-
-                <div className={cx('mySlides', 'fade')}>
-                    <img alt='img-3' src={slideImg3}/>
-                </div>
-            </div>
-
+            <Box overflow={'hidden'}>
+                <Carousel play={'5000'}>
+                    <Image src={slideImg1}/>
+                    <Image src={slideImg2}/>
+                    <Image src={slideImg3}/>
+                </Carousel>
+            </Box>
         </div>
     )
 }
